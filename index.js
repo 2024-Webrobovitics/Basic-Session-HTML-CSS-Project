b7d6ccd3b9737190214cd31ecb8aa5c39b3d9ef6
2024-Webrobovitics <161070703+2024-Webrobovitics@users.noreply.github.com>
2024-03-08 17:27:06 +0530

    Update temp.html

--- index.js
+++ index.js
@@ -1,39 +1,20 @@
-const idElement=document.getElementById("three");
-const classElements=document.getElementsByClassName("two");
-const heading=document.getElementsByTagName("h1");
-
-const classArray=Array.from(classElements);
-
-idElement.style.backgroundColor="yellow";
-
-classArray.forEach((c)=>{
-    c.style.backgroundColor="red";
-})
-
-classArray[0].classList.add("invisible");
-classArray[0].classList.remove("invisible");
-
-idElement.innerHTML="<p>Hello World</p><a href='#'>Link</a>";
-
-idElement.querySelector("a").setAttribute("href","https://www.google.com/")
-
-const arr=Array.from(heading);
-arr[0].textContent="Hello Nipun";
-
-idElement.addEventListener("click",function(){
-    classArray[2].style.backgroundColor="violet";
-    classArray[1].classList.toggle("invisible");
-})
-
-const newClassArray=document.querySelectorAll(".one");
-newClassArray.forEach((c)=>{
-    c.addEventListener("click",function(){
-        c.textContent="Hello World";
-    })
-})
-
-document.addEventListener("keypress",function(event){
-    if(event.key=='k'){
-        document.querySelector(".one").style.backgroundColor="green";
-    }
-})
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="temp.css">
+</head>
+<body>
+    <h1>Hello World</h1>
+    <div class="one"></div>
+    <div class="one"></div>
+    <div class="one"></div>
+    <div class="two"></div>
+    <div class="two"></div>
+    <div class="two"></div>
+    <div id="three"></div>
+    <script src="index.js"></script>
+</body>
+</html>
